eedd5eadfbf9fde4263b4a510d7d908885fd6d2d
remi <rachelmilb89@gmail.com>
2024-06-20 15:24:56 -0400

    CH7 HW V1

--- stringing-characters-together/exercises/part-one.js
+++ stringing-characters-together/exercises/part-one.js
@@ -1,10 +1,29 @@
 let num = 1001;
 
-//Returns 'undefined'.
+//Returns 'undefined'. DOES NOT GIVE LENGTH OF A NUMBER
 console.log(num.length);
 
 //Use type conversion to print the length (number of digits) of an integer.
 
+let stringedNumber = num.toString();
+let lengthOfNum = stringedNumber.length;
+console.log(stringedNumber.length);
+
+
 //Follow up: Print the number of digits in a DECIMAL value (e.g. num = 123.45 has 5 digits but a length of 6).
+//THE PERIOD COUNTS IN THE LENGTH and must be modified to remove the period
+let decVal = 1234.56
+let decValString = decVal.toString();
+let lengthOfD = decVal.length;
+console.log(decValString.length);
+
+let decValWithout = decValString.replace('.', '');//removed the period from a string
+console.log(decValWithout.length);//prints string excluding period
+
 
 //Experiment! What if num could be EITHER an integer or a decimal?  Add an if/else statement so your code can handle both cases.
+if (String(num).includes('.')){
+    console.log(String(num).length-1);
+    } else {
+    console.log(String(num).length);
+    }
--- stringing-characters-together/exercises/part-three.js
+++ stringing-characters-together/exercises/part-three.js
@@ -4,14 +4,34 @@ let language = 'JavaScript';
 
 //1. Use string concatenation and two slice() methods to print 'JS' from 'JavaScript'
 
+console.log(language.slice(0,1));//could also make a variable for j and s and then add them together
+console.log(language.slice(4,5));//you would could add them together using ${} or print the two variables
+
+
+console.log(language.slice(0,1) + language.slice(4,5));
+
 //2. Without using slice(), use method chaining to accomplish the same thing.
 
+console.log(language.substring(0,1));
+console.log(language.substring(4,5));
+
+let initials = (language.substring(0,1) + language.slice(4,5));
+console.log(initials);
+
 //3. Use bracket notation and a template literal to print, "The abbreviation for 'JavaScript' is 'JS'."
+//kind of confused here idk why the brackets keep printing?
+//is the solution wrong? or mu understanding of bracket notation is not correct?
+console.log("The abbreviation for [" + language + "] is [" + initials + "].");
 
 //4. Just for fun, try chaining 3 or more methods together, and then print the result.
 
+let forFun = (language.slice(0,1) + language.substring(4,5) + ", " + language.length + ", " + language.toLowerCase());
+console.log(forFun);
 //Part Three section Two
 
 //1. Use the string methods you know to print 'Title Case' from the string 'title case'.
 
 let notTitleCase = 'title case';
+let realCase = notTitleCase.replace("title case", "Tital Case");
+
+console.log(realCase);
